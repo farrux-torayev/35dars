ffcbd52f96d8f8a3d32b1e84f84912adb514c1c6
farrux-torayev <farruxtorayev590@gmail.com>
2024-11-29 23:43:56 +0500

    35-dars

---
+++ js/index.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      width: {
+        DEFAULT: "100%",
+        desktop: "1312px",
+      },
+      padding: {
+        DEFAULT: "20px",
+      },
+    },
+    screens: {
+      DEFAULT: "100%",
+      desktop: "1312px",
+    },
+    extends: {
+      colors: {},
+    },
+  },
+};
